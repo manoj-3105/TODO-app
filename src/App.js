d861a8d72b72645eb6287b6a8b69b1a3148e2cf8
Manoj <manojdom84@gmail.com>
2025-04-02 15:44:04 +0530

    React todo-app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Todolists from './components/Todolists';
-import New from './components/New';
 function App() {
   return (
     <div>
     <h1>React TODO project</h1>
-    <New/>
     <Todolists/>
     </div>
   );
