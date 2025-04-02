27e90b77e640bf2f99f9d411ccb23fffc7e3ba7b
Priyanka Palikaa <priyankapalikaa@Priyankas-MacBook-Air.local>
2025-04-02 15:15:50 +0530

    added new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
-
 import React from 'react';
 import Todolists from './components/Todolists';
+import New from './components/New';
 function App() {
   return (
     <div>
+    <h1>React TODO project</h1>
+    <New/>
     <Todolists/>
     </div>
   );
