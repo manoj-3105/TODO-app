a6788753ab125d9f2773993fadb16c25a0b503c2
Priyanka Palikaa <priyankapalikaa@Priyankas-MacBook-Air.local>
2025-04-02 15:00:59 +0530

    The Todo application is a user-friendly task management tool built with React.js, utilizing Formik for form handling, Yup for form validation, Axios for API communication, and Framer Motion for smooth animations. It provides a clean and intuitive interface for users to create, view, update, and delete their tasks.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+import Todolists from './components/Todolists';
+function App() {
+  return (
+    <div>
+    <Todolists/>
+    </div>
+  );
+}
+
+export default App;
